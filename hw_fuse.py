37ba0627aaca739a652b9ab9710d5e5676cea7ba
kai93646 <kebin64639@gmail.com>
2024-12-29 21:15:15 +0800

    簡易file system實做

---
+++ hw_fuse.py
@@ -0,0 +1,95 @@
+from fuse import FUSE, FuseOSError, Operations
+from time import time
+from stat import S_IFDIR, S_IFREG, S_IFLNK      #S_IFDIR: directory, S_IFREG: regular file, S_IFLNK: symbolic link
+from errno import ENOENT, EEXIST
+from collections import defaultdict
+from sys import argv
+
+class SimpleFS(Operations):
+    def __init__(self):
+        self.metadata:dict[str, bytes] = {}
+        self.data = defaultdict(bytes)      #if the index reqired is not exist in dict, then print the null value
+        self.fd = 0                         #file descriptor
+        now = time()
+        self.metadata['/'] = dict(st_mode=(S_IFDIR | 0o755), st_nlink=2, st_ctime=now, st_mtime=now, st_atime=now) 
+        #st_ctime: creation time, st_mtime: modification time, st_atime: access time
+        '''
+        print(bin(self.metadata['/'][str('st_mode')]) , bin(S_IFDIR))
+        print()
+        '''
+
+#create, write and remove file
+    def create(self, path, mode):
+        print('create', path)
+        self.metadata[path] = dict(st_mode=(S_IFREG | mode), st_nlink=1, st_size=0, st_ctime=time(), st_mtime=time(), st_atime=time())
+        self.fd += 1
+        return self.fd
+
+    def read(self, path, size, offset, fh):
+        return self.data[path][offset:offset + size]        #catch the index of path and read the data from offset to size
+
+    def write(self, path, buf, offset, fh):
+        self.data[path] = (
+            # make sure the data gets inserted at the right offset
+            self.data[path][:offset].ljust(offset, '\x00'.encode('ascii'))
+            + buf
+            # and only overwrites the bytes that data is replacing
+            + self.data[path][offset + len(buf):])
+            #insert the data in buffer from offset to offset+len
+        self.metadata[path]['st_size'] = len(self.data[path]) #update the size of the file
+        self.metadata[path]['st_mtime'] = time()              #update the modification time
+        return len(buf)
+
+    def open(self, path, flags):
+        print('open', path, flags)
+        self.fd += 1
+        self.metadata[path]['st_atime'] = time()
+        return self.fd
+
+    def release(self, path, fd):
+        print('release', path, fd)
+        return 0
+
+    def truncate(self, path, length, fd=None):
+        print('truncate', path, length)
+        self.data[path] = self.data[path][:length] #divide the path's size to length
+        self.metadata[path]['st_size'] = length    #specify the size of the file
+        self.metadata[path]['st_mtime'] = time()
+
+
+#create and remove directory
+    def mkdir(self, path, mode):
+        self.metadata[path] = dict(
+            st_mode=(S_IFDIR | mode),
+            st_nlink=2,
+            st_size=0,
+            st_ctime=time(),
+            st_mtime=time(),
+            st_atime=time())
+        self.metadata['/']['st_nlink'] += 1
+
+    def rmdir(self, path):
+        # with multiple level support, need to raise ENOTEMPTY if contains any files
+        self.metadata.pop(path)
+        self.metadata['/']['st_nlink'] -= 1
+
+#list directory contents
+    def readdir(self, path, fh):
+        return ['.', '..'] + [x[1:] for x in self.metadata if x != '/']
+
+#file and directory attributes
+    def getattr(self, path, fh=None):
+        print('getattr', path)
+        if path not in self.metadata:
+            print('raise FuseOSError(ENOENT)')
+            raise FuseOSError(ENOENT)
+        return self.metadata[path]
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mount')
+    args = parser.parse_args()
+
+    fuse = FUSE(SimpleFS(), args.mount, foreground=True)
